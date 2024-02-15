337b2233a88f75b06d08e9f99b6b18e003658213
Leandro López (inkel) <leandro.lopez@grafana.com>
2024-02-15 14:48:03 -0300

    Add basic package documentation

--- level.go
+++ level.go
@@ -1,3 +1,26 @@
+// Copyright 2024 Leandro "inkel" López. All rights reserved.
+
+/*
+Package slogflag implements command-line flag parsing for slog.Level
+variables.
+
+# Usage
+
+Define flags using [slogflag.Level]. This declares a slog.Level flag, -log-level, stored in the pointer level, with type *slog.Level:
+
+	var level = slogflag.Level("log-level", slog.LevelInfo, "help message for flag -log-level")
+
+If you like, you can bind the flag to a variable using the [slogflag.LevelVar] function:
+
+	var levelVar slog.Level
+	func init() {
+		slogflag.LevelVar(&levelVar, "log-level", slog.LevelInfo, "help message for flag -log-level")
+	}
+
+After all the flags are defined, call [flag.Parse] as usual. Then you can set the log level of your [slog.Logger] like this:
+
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: *level}))
+*/
 package slogflag
 
 import (
@@ -13,6 +36,8 @@ type levelVar slog.Level
 var _ flag.Value = new(levelVar)
 var _ slog.Leveler = new(levelVar)
 
+// ErrParse is the error returned when it's impossible to parse the
+// log level to something slog accepts.
 var ErrParse = errors.New("cannot parse log level")
 
 func parse(val string) (slog.Level, error) {
@@ -66,12 +91,18 @@ func (lv *levelVar) String() string {
 
 func (lv *levelVar) Level() slog.Level { return slog.Level(*lv) }
 
+// LevelVar defines a [slog.Level] flag with specified name, default
+// value, and usage string. The argument p points to a [slog.Level]
+// variable in which to store the value of the flag.
 func LevelVar(p *slog.Level, name string, value slog.Level, usage string) {
 	*p = value
 	lv := levelVar(*p)
 	flag.CommandLine.Var(&lv, name, usage)
 }
 
+// Level defines a [slog.Level] flag with specified name, default
+// value, and usage string. The return value is the address of a
+// [slog.Level] variable that stores the value of the flag.
 func Level(name string, value slog.Level, usage string) *slog.Level {
 	var l slog.Level
 	LevelVar(&l, name, value, usage)
